e6c17ea5629fd4facecf585b9d297f215a371a19
GooGuJiang <gu@gmoe.cc>
2024-10-26 11:49:16 +0800

    ✔ 提交代码

---
+++ 哇，是排序算法/main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+void printArray(int arry[],int len){
+	int i;
+	for(i=0;i<len;i++){
+		printf("%d ",arry[i]);
+	}
+	printf("\n");
+}
+
+void instertSort(int arry[],int len){
+	int i,j;
+	for(i=1;i<len;i++){
+		int insertNumber=arry[i];
+		for(j=i-1;j>=0;j--){
+			if(arry[j]<insertNumber)
+			break;
+			arry[j+1]=arry[j];
+		}
+		arry[j+1]=insertNumber;
+	}
+}
+
+int main(){
+	int nums[]={4,3,1,3,2};
+	printArray(nums,5);
+	instertSort(nums,5);
+	printArray(nums,5);
+}
